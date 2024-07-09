cbc3bf0591127ff408ebe8a2f4d83fe4dd43b2ea
siobhanschenk <sschenk@uwo.ca>
2024-07-08 17:56:10 -0700

    removed carousel

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Initially show the first tab
-    showTab('tab-about');
-
-    // Add event listeners to navigation links
-    const navLinks = document.querySelectorAll('header nav ul li a');
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const tabName = this.getAttribute('href').substring(1); // Get tab ID from href
-            showTab(tabName);
-            window.location.hash = tabName; // Update URL hash
-        });
-    });
-});
-
-function showTab(tabName) {
+function showTab(tabId) {
     const tabs = document.querySelectorAll('.tab-content');
     tabs.forEach(tab => {
-        if (tab.id === tabName) {
+        if (tab.id === tabId) {
             tab.classList.add('active');
         } else {
             tab.classList.remove('active');
