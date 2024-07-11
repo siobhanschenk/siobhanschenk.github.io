68542507ce83bd5c7f142b07619b9b187b82868c
Siobhan Schenk <35807279+siobhanschenk@users.noreply.github.com>
2024-07-10 20:39:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Initially show the first tab
+    // Initially show the About Me tab
     showTab('tab-about');
 
     // Add event listeners to navigation links
@@ -7,17 +7,17 @@ document.addEventListener("DOMContentLoaded", function () {
     navLinks.forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault();
-            const tabName = this.getAttribute('href').substring(1); // Get tab ID from href
+            const tabName = this.getAttribute('onclick').replace("showTab('", "").replace("')", "");
             showTab(tabName);
             window.location.hash = tabName; // Update URL hash
         });
     });
 });
 
-function showTab(tabId) {
+function showTab(tabName) {
     const tabs = document.querySelectorAll('.tab-content');
     tabs.forEach(tab => {
-        if (tab.id === tabId) {
+        if (tab.id === tabName) {
             tab.classList.add('active');
         } else {
             tab.classList.remove('active');
